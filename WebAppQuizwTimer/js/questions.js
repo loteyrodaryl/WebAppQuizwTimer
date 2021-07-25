4a9ecb997e4198d74a22f22cacd02bc72c32c027
Daryl Loteyro <83477798+loteyrodaryl@users.noreply.github.com>
2021-07-24 17:27:39 -0700

    Add files via upload
    initial commit

---
+++ WebAppQuizwTimer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT INCLUDE",
+    answer: "alerts",
+    options: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if / else statement is enclosed within _____",
+    answer: "parenthesis",
+    options: [
+      "quotes",
+      "curly brackets",
+      "parenthesis",
+      "square brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store ______?",
+    answer: "all of the above",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "console.log",
+    options: [
+      "JavaScript",
+      "terminal / bash",
+      "for loops",
+      "console.log"
+    ]
+  },
+    {
+    numb: 5,
+    question: "String values must be enlosed within ______ when being assigned to variables.",
+    answer: "curly brackets",
+    options: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parenthesis"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ WebAppQuizwTimer/readme.md
@@ -0,0 +1,11 @@
+Engage your audience
+Engage your audience in a unique and fun way and connect them to your brand or learning material. By creating a returning event, you will improve your consumer loyalty.
+
+Large number
+Taking online quizzes makes it possible to have a large number of participants. It could be up to a number of 1000 participants (and even more!). It doesn’t matter at what kind of location they take the online quiz as long as they are connected to the Internet.
+
+Randomizing questions
+It is significant more easy to randomize your question with just one click than to do it all manually. Randomizing questions and even answers of those questions is not a lot a of work to do with online quizzes. Besides the advantage of time saving, it also helps preventing students from cheating.
+
+Quiz results/ gain insight in audience
+Another plus of online quizzes is getting immediate results from your participants. Not only does the creator get the results, the participant itself is also possible to get his or her results. This makes it for the creator easy to identify the gaps in knowledge. The participants are able to view on what kind of section they have to improve themselves.
